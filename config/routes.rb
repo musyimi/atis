df926aaf3c43abe39a2b21d9bd263c061405c95d
moses musyimi <pmusyimi13@gmail.com>
2021-08-31 08:32:18 +0300

    add pretty testing display

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'html_pages#home'
   get 'html_pages/about'
+  get 'html_pages/home'
   get 'html_pages/experience'
   get 'html_pages/casestudies'
   get 'html_pages/resources'
