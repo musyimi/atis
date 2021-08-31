edd1958e01518205893683e1d447ebb38d6f18a5
moses musyimi <pmusyimi13@gmail.com>
2021-08-31 08:52:49 +0300

    helper basics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = '')
+        base_title = "Atis Telcom"
+        if page_title.empty?
+            base_title
+        else
+            page_title + "|" + base_title
+        end
+    end
 end
